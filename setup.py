6105c5a77858476532f98228c1ab92fd8f84d5c3
Kukushkin Alexander <alex@alexkuk.ru>
2018-06-28 17:14:24 +0300

    Publish to Pypi

--- setup.py
+++ setup.py
@@ -4,8 +4,23 @@ from setuptools import setup, find_packages
 setup(
     name='ipymarkup',
     version='0.1.0',
+    description='NER markup visualisation for Jupyter Notebook',
+    url='https://github.com/natasha/ipymarkup',
+    author='Alexander Kukushkin',
+    author_email='alex@alexkuk.ru',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='ner, markup, jupyter, ipython',
+    packages=find_packages(),
     install_requires=[
         'intervaltree==2.1.0'
-    ],
-    packages=find_packages()
+    ]
 )
