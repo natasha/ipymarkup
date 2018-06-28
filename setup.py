fdd41adf8f4aee4c3bb3f136fea16eb07ddff5ee
Kukushkin Alexander <alex@alexkuk.ru>
2018-06-28 17:30:32 +0300

    Add desction for Pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,42 @@
 
 from setuptools import setup, find_packages
 
+
+description = '''
+ipymarkup 
+=========
+
+NER markup visualisation for Jupyter Notebook. 
+
+::
+
+  from ipymarkup import Span, AsciiMarkup
+
+  text = 'a d a b a a a b c c c f d'
+  spans = [
+    Span(0, 13, 'a'),
+    Span(2, 25, 'd'),
+    Span(6, 15, 'b'),
+    Span(16, 21, 'c'),
+    Span(22, 23, 'f'),
+  ]
+  AsciiMarkup(text, spans)
+
+  >>> a d a b a a a b c c c f d
+  ... a------------ c---- f 
+  ... d----------------------
+  ... b-------- 
+
+
+For more examples and explanation see `ipymarkup documentation <http://nbviewer.jupyter.org/github/natasha/ipymarkup/blob/master/docs.ipynb>`_.
+'''
+
 setup(
     name='ipymarkup',
-    version='0.2.0',
+    version='0.2.2',
     description='NER markup visualisation for Jupyter Notebook',
+    long_description=description,
+    long_description_content_type='text/x-rst',
     url='https://github.com/natasha/ipymarkup',
     author='Alexander Kukushkin',
     author_email='alex@alexkuk.ru',
