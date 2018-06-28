4b6909efc96be1485d8f7ec34a535038a00bfa8b
Kukushkin Alexander <alex@alexkuk.ru>
2018-06-28 17:16:04 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ipymarkup',
-    version='0.1.0',
+    version='0.2.0',
     description='NER markup visualisation for Jupyter Notebook',
     url='https://github.com/natasha/ipymarkup',
     author='Alexander Kukushkin',
