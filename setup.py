1cce51c90ec760cb292e63b13e4702dbb1817c7d
Kukushkin Alexander <alex@alexkuk.ru>
2018-08-10 15:36:30 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ For more examples and explanation see `ipymarkup documentation <http://nbviewer.
 
 setup(
     name='ipymarkup',
-    version='0.2.2',
+    version='0.2.3',
     description='NER markup visualisation for Jupyter Notebook',
     long_description=description,
     long_description_content_type='text/x-rst',
