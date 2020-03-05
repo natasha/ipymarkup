0adcea69a2458100a12749f29ac14fd209124b01
Kukushkin Alexander <alex@alexkuk.ru>
2020-03-05 16:26:05 +0300

    Up api

--- ipymarkup/__init__.py
+++ ipymarkup/__init__.py
@@ -1,2 +1,13 @@
 
-from .markup import *  # noqa
+from .ner import format_ner_box_markup, show_ner_box_markup  # noqa
+from .ner import format_ner_line_markup, show_ner_line_markup  # noqa
+from .ner import format_ner_ascii_markup, show_ner_ascii_markup  # noqa
+
+from .dep import format_dep_markup, show_dep_markup  # noqa
+from .dep import format_dep_ascii_markup, show_dep_ascii_markup  # noqa
+
+
+# legacy
+show_box_markup = show_ner_box_markup
+show_line_markup = show_ner_line_markup
+show_ascii_markup = show_ner_ascii_markup
